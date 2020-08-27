a4c4aa710b75f45203c88ac0fcb4370c047f7070
Jonathan Kevin <jonathan.kevinbp@hotmail.com>
2020-08-27 00:19:04 -0300

    Criação de loader de models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Jonathan Kevin',
+    email: 'jonathan.k@hotmail.com',
+    password_hash: '12378965',
+  });
+  return res.json(user);
 });
 
 export default routes;
