989d0d141740fae77b160e53f1435f66c7569226
Jonathan Kevin <jonathan.kevinbp@hotmail.com>
2020-10-25 01:09:36 -0300

    Configuração de email

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c3c8a7e6101537',
+    pass: '310d7a83f9c4a2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
